ca84fd9b6b113ab8b2d9a82fc5daa4e756a22c23
LeoParraG17 <163213450+LeoParraG17@users.noreply.github.com>
2024-09-12 13:44:14 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+// const arreglo =[1,2,3,4];
+// for (i = 0;i < arreglo.length; i++ ){
+//     console.log(arreglo[i]);
+// }
+
+// arreglo.forEach(function(posicion){
+//     console.log(posicion);
+
+// })
+
+const titulo = "Bienvenidos a Salento, Quindio";
+const etiquetaTitulo = document.querySelector("header H1");
+
+const lugaresInteres = [
+
+{ 
+    "Titulo": "Valle del Cocora",
+    "descripcion":  "El Valle de Cocora es el hogar de las famosas palmas de cera, el árbol nacional de Colombia. Este impresionante valle ofrece rutas de senderismo entre montañas y paisajes verdes, donde se pueden apreciar las palmas más altas del mundo."
+
+},
+
+{ 
+    "Titulo": "Calle Real",
+    "descripcion":  "Calle Real es la principal calle peatonal de Salento, conocida por sus coloridas casas coloniales, tiendas de artesanías, y cafés acogedores. Es un lugar perfecto para pasear, disfrutar de la cultura local, y comprar recuerdos."
+},
+
+{ 
+    "Titulo": "Mirador de Salento",
+    "descripcion":  "El Mirador de Salento ofrece vistas panorámicas del Valle de Cocora y el paisaje circundante. Es un lugar ideal para capturar fotos impresionantes y disfrutar de la belleza natural del Quindío."
+},
+
+];
